a378372d80b48d75d1f4c0502d6fe78b282f85b8
Winston Wu <winstwu@gmail.com>
2013-02-15 15:42:21 -0500

    Add Sign up tests and exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,14 @@ describe "UserPages" do
 		  it "should not create a user" do
 		    expect { click_button submit }.not_to change(User, :count)
 		  end
+
+		  describe "after submission" do
+		  	before { click_button submit }
+
+		  	it { should have_selector('title', text: 'Sign up') }
+		  	it { should have_content('error') }
+		  	it { should_not have_content('Password digest') }
+		  end
 		end
 
 		describe "with valid information" do
@@ -44,6 +52,7 @@ describe "UserPages" do
 				before { click_button submit }
 				let(:user) { User.find_by_email("user@example.com") }
 				it { should have_selector('title', text: user.name) }
+				it { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end	 
 	end
